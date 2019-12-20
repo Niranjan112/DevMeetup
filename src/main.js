ef09e91b4f0459df6412ddd4e108e6e61f0a39c1
niranjan112 <niranjan112>
2019-12-20 19:56:51 +0530

    added firebase and created sign up form

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify'
 import DateFilter from './filter/date'
+import * as firebase from 'firebase'
 
 Vue.config.productionTip = false
 Vue.filter('date', DateFilter)
@@ -13,5 +14,16 @@ new Vue({
   router,
   store,
   vuetify,
-  render: h => h(App)
+  render: h => h(App),
+  created () {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyBejumyEETORIkaKL6JNGUmBNtO6mGw-yU',
+      authDomain: 'devmeetup-f132f.firebaseapp.com',
+      databaseURL: 'https://devmeetup-f132f.firebaseio.com',
+      projectId: 'devmeetup-f132f',
+      storageBucket: 'devmeetup-f132f.appspot.com',
+      messagingSenderId: '358639659596',
+      appId: '1:358639659596:web:0f67d4cfc3e017dd35a2e9'
+    })
+  }
 }).$mount('#app')
--- src/views/User/Signup.vue
+++ src/views/User/Signup.vue
@@ -1,5 +1,71 @@
 <template>
-  <div>
-    This is Sign up
-  </div>
+  <v-container>
+    <v-row justify="center">
+      <v-col cols="12" sm="6">
+        <v-card>
+          <v-card-text>
+            <v-container>
+              <v-form>
+                <v-row>
+                  <v-col cols="12">
+                    <v-text-field
+                      name="email"
+                      label="Mail"
+                      id="email"
+                      v-model="email"
+                      type="email"
+                      required
+                    ></v-text-field>
+                    <v-text-field
+                      :append-icon="showPassword1 ? 'mdi-eye' : 'mdi-eye-off'"
+                      :type="showPassword1 ? 'text' : 'password'"
+                      @click:append="showPassword1 = !showPassword1"
+                      name="password"
+                      label="Password"
+                      id="password"
+                      v-model="password"
+                      required
+                    ></v-text-field>
+                    <v-text-field
+                      :append-icon="showPassword2 ? 'mdi-eye' : 'mdi-eye-off'"
+                      :type="showPassword2 ? 'text' : 'password'"
+                      @click:append="showPassword2 = !showPassword2"
+                      name="confirmPassword"
+                      label="Confirm Password"
+                      id="confirmPassword"
+                      v-model="confirmPassword"
+                      :rules="[rules.comparePasswords]"
+                    ></v-text-field>
+                    <v-btn type="submit">Sign Up</v-btn>
+                  </v-col>
+                </v-row>
+              </v-form>
+            </v-container>
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
+
+<script>
+export default {
+  data () {
+    return {
+      email: '',
+      password: '',
+      confirmPassword: '',
+      showPassword1: false,
+      showPassword2: true,
+      rules: {
+        comparePasswords: value => value === this.password || 'Password do not match'
+      }
+    }
+  },
+  methods: {
+    onSignUp () {
+      console.log({ email: this.email, password: this.password, confirmPassword: this.confirmPassword })
+    }
+  }
+}
+</script>
